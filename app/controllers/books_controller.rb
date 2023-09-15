24adde253947623ace0b5bc94f719c302cb2db08
EC2 Default User <ec2-user@ip-172-31-29-191.ap-northeast-1.compute.internal>
2023-09-15 12:56:11 +0000

    [Add]4-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -39,6 +40,6 @@ class BooksController < ApplicationController
   private
   #ストロングパラメータ
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
